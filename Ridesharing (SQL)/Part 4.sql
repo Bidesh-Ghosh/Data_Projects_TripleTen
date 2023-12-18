1e38b891866b54036f051fad4803ed8e7e7bc95e
Bidesh Ghosh <ovighosh91@gmail.com>
2023-12-18 15:31:26 -0500

    Delete Ridesharing (SQL) directory

--- Ridesharing (SQL)/Part 4.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    neighborhoods.neighborhood_id AS neighborhood_id,
-    neighborhoods.name AS name
-FROM
-    neighborhoods
-WHERE
-    name LIKE '%Hare%'
-    OR name = 'Loop'
--- Ridesharing (SQL)/Part 5.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    weather_records.ts AS ts,
-    CASE WHEN weather_records.description LIKE '%rain%' THEN 'Bad'
-    WHEN weather_records.description LIKE '%storm%' THEN 'Bad'
-    ELSE 'Good'
-    END AS weather_conditions
-FROM
-    weather_records
